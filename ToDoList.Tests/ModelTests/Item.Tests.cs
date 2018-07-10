12bad5ea6c967f75629fc4409d6fef3a45a1f2b3
Kelli Brooke, Matt Smith and Ryan Putman <matt@>
2018-07-10 09:13:44 -0700

    initial commit

---
+++ ToDoList.Tests/ModelTests/Item.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class ItemTest
+    {
+
+    }
+}
---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using ToDoList;
+
+namespace ToDoList.Models
+{
+    public class Item
+    {
+
+    }
+}
